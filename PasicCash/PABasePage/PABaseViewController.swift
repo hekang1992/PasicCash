d18404bde99b69fec8d8c2e6f828cec6150c702d
何康 <hekang19921111@hotmail.com>
2024-07-02 16:33:15 +0800

    add:新增项目目录结构

---
+++ PasicCash/PABasePage/PABaseViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PABaseViewController.swift
+//  PasicCash
+//
+//  Created by apple on 2024/7/2.
+//
+
+import UIKit
+
+class PABaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
